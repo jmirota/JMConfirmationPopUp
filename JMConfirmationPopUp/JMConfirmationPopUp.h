ee2c0866d5971abead3c8539390c4e7c5c90ed3e
Jakub Mirota <jmirota@future-processing.com>
2017-11-12 21:06:39 +0100

    Initial Commit

---
+++ JMConfirmationPopUp/JMConfirmationPopUp.h
@@ -0,0 +1,19 @@
+//
+//  JMConfirmationPopUp.h
+//  JMConfirmationPopUp
+//
+//  Created by Jakub Mirota on 12.11.2017.
+//  Copyright © 2017 Jakub Mirota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JMConfirmationPopUp.
+FOUNDATION_EXPORT double JMConfirmationPopUpVersionNumber;
+
+//! Project version string for JMConfirmationPopUp.
+FOUNDATION_EXPORT const unsigned char JMConfirmationPopUpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JMConfirmationPopUp/PublicHeader.h>
+
+
